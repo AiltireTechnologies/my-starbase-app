c4f8b291a59e5982883b20f1c6c7838e27e56d56
Ailtire <admin@ailtire.io>
2025-03-27 00:15:44 +0000

    initial Starbase + Vite setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Heading, Button } from '@jupiterone/starbase';
+
+export default function App() {
+  return (
+    <>
+      <Heading level={1}>Starbase in Azure 🚀</Heading>
+      <Button intent="primary">Click me</Button>
+    </>
+  );
+}
+
